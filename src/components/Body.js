0bc0dd0b90d07ff4390d574c3c49ff0e6cf19d85
Deepak Maddheshiya <maddheshiya.deepak@gmail.com>
2023-03-02 21:05:44 +0530

    Netlify File

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,3 +12,35 @@ const Body = () => {
 };
 
 export default Body;
+
+let cart = ["Watch", "Glasses"];
+createOrder(cart)
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+function createOrder(cart) {
+  return new Promise(function (resolve, reject) {
+    if (!validCart()) {
+      const err = new Error("Invalid Cart!");
+      reject(err);
+    }
+    const orderId = "12345";
+    if (orderId) {
+      resolve(orderId);
+    }
+  });
+}
+function proceedToPayment(paymentInfo) {
+  return new Promise((resolve, reject) => {
+    reject("Payment Failed - " + paymentInfo);
+  });
+}
+function validCart() {
+  return true;
+}
